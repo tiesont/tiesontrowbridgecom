f7752d486be16f5345f0a88b09e6e696f2e416c2
Tieson Trowbridge <ttrowbridge@arivium.com>
2025-05-02 00:17:55 -0400

    Tweak to random-quote script

--- docs/js/random-quote.js
+++ docs/js/random-quote.js
@@ -4,7 +4,7 @@ $(function(){
     let i = Math.floor(Math.random() * quoteIDs.length);
     let id = quoteIDs[i];
 
-    let url = `/quotes/${id}`;
+    let url = `${location.origin}/quotes/${id}`;
 
     $.get(url)
         .done(function(response, status, jqxhr){
